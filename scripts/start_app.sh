ff2afa85fc8ec20c100d19fb7f804aaab3c218e5
Anderson Orobor <orobor.ise@gmail.com>
2023-12-09 18:28:00 +0100

    config update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,19 +3,8 @@
 sed -i 's/\[]/\["54.244.198.169"]/' /home/ubuntu/pipelineapp/pipelineapp/settings.py
 
 python manage.py migrate 
-# python manage.py makemigrations     
+#python manage.py makemigrations     
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
-#sudo tail -f /var/log/nginx/error.log
-#sudo systemctl reload nginx
-#sudo tail -f /var/log/nginx/error.log
-#sudo nginx -t
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
-# Check the status
-#systemctl status gunicorn
-# Restart:
-#systemctl restart gunicorn
-#sudo systemctl status nginx
+
