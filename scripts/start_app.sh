ec5a78a3a50cfe437a299b212043ed686ce519ff
Anderson Orobor <orobor.ise@gmail.com>
2023-12-09 21:06:42 +0100

    config update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.244.198.169"]/' /home/ubuntu/pipelineapp/pipelineapp/settings.py
+sed -i 's/\[]/\["13.53.124.122"]/' /home/ubuntu/pipelineapp/pipelineapp/settings.py
 
 python manage.py migrate 
-#python manage.py makemigrations     
+python manage.py makemigrations     
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
